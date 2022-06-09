561e7f2c36289da1c702a045681cc5fc4fa95926
NazmiSam <m.nazmi124@gmail.com>
2022-06-09 15:04:29 +0800

    init

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  
+  
+  devise_scope :user do
+    # Redirests signing out users back to sign-in
+    get "users", to: "devise/sessions#new"
+  end
+  
+  devise_for :users
+  
+  root 'home#index'
+  
+end
