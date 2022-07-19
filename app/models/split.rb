c9ca631158bffe12e9d0bca23fbdaa124a5eed0e
Nazmi Sam <m.nazmi124@gmail.com>
2022-07-19 02:45:05 +0000

    update split

--- app/models/split.rb
+++ app/models/split.rb
@@ -2,14 +2,7 @@ class Split < ApplicationRecord
   belongs_to :payment
 
   validate :percentage_rate
-  before_validation :generate_split_settlement, on: :create
-
-  def generate_split_settlement
-    begin
-      self.account = 
-    end while self.class.find_by(order_number: order_number)
-  end
-
+ 
 
   def percentage_rate  
     rate = product.splits.sum(:split_percent)
